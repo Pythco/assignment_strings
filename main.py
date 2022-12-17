f8afdf2bc764448409d05ec47438b7c24e93a585
Pythco <112080854+Pythco@users.noreply.github.com>
2022-12-17 19:44:52 +0100

    Add files via upload
    Changed according feedback!

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ goal_0 = 32
 goal_1 = 54
 scorers = f'{player_1} {goal_0}, {player_2} {goal_1}'
 print(scorers)
-report = f'{player_1} scored in the {goal_0}nd minute' '\n' f'{player_2} scored in the {goal_1}th minute'
+report = f'{player_1} scored in the {goal_0}nd minute\n{player_2} scored in the {goal_1}th minute'
 print(report)
 player = "Ronald Koeman"
-first_name = "Ronald"
-print(first_name)
 last_name_len = len(player) - player.find("Koeman")
 print(last_name_len)
-name_short = player[0]+ ". " + player[7:14]
+index_of_space= player.find(" ")
+name_short = player[0]+ ". " + player[index_of_space+1:]
 print(name_short)
-chant = "Ronald! " * 5 +"Ronald!"
+first_name = player[0:index_of_space]
+len_first_name = len(first_name)
+print(first_name)
+chant = (first_name + "! ") * (len_first_name -1) + (first_name + "!")
 print(chant)
 good_chant = (chant[-1]!= " ")
 print(good_chant)
